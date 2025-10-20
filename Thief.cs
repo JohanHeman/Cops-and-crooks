e364039ae906d3abfb833ee0cb1fbd25fff7c378
Oliver Lidberg <oliverlidberg@gmail.com>
2025-10-20 13:27:42 +0200

    changed thief and police

--- Thief.cs
+++ Thief.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Thief
+    internal class Thief : Person
     {
+        public int TimeInPrison { get; set; }
+
+
+        public Thief(int timeInPrison, string name, int positionX, int positionY, List<Item> inventory, int directionX, int directionY) : base(name, positionX, positionY, inventory, directionX, directionY)
+        {
+            TimeInPrison = timeInPrison;
+        }
+
+        public override TransferBetweenInventory()
+        {
+            
+        }
+
+        public void SendToPrison()
+        {
+            //hejadad
+        }
     }
 }
