02fda9810e8ae2e69c4676dad28bf2cf1f427806
Johan Heman <jojo2001@hotmail.se>
2025-10-20 14:34:05 +0200

    fixed police

--- Police.cs
+++ Police.cs
@@ -8,6 +8,18 @@ namespace ConsoleApp1
 {
     internal class Police : Person
     {
+
+
+
+        public Police(string name, int positionX, int positionY, List<Item> inventory, int directionX, int directionY) : base(name, positionX, positionY, inventory, directionX, directionY)
+        {
+            
+        }
+
+
+
+
+
         public override void TransferBetweenInventory(Person person1, Person person2)
         {
             base.TransferBetweenInventory(person1, person2);
