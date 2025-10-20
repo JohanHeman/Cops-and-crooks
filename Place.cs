cddd97c980c050a466d3ae99845e4dac1ca1a517
seventh <mikael.smile@hotmail.se>
2025-10-20 09:21:27 +0200

    Place Base 'Functions'
    The base of place with only the most basic of functionality, aka it has the Draw() and CheckCollision().
    But with unfinished code for both and only the most rudementary version of it.

--- Place.cs
+++ Place.cs
@@ -8,5 +8,50 @@ namespace ConsoleApp1
 {
     internal class Place
     {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public List<Person> People { get; set; }
+
+        public Place(string name, int size)
+        {
+            Name = name;
+            Size = size;
+            People = new List<Person>();
+        }
+        public void Draw()
+        {
+            int x = 0;
+            int y = 0;
+
+            while (y < Size + 1)
+            {
+                if (x < Size + 1)
+                {
+                    if (x == 0 || x == Size)
+                    {
+                        Console.Write("|");
+                    }
+                    else if (y == 0 || y == Size)
+                    {
+                        Console.Write("---");
+                    }
+                    else
+                    {
+                        Console.Write(" X ");
+                    }
+                    x++;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    x = 0;
+                    y++;
+                }
+            }
+        }
+        public bool CheckCollision()
+        {
+            return false;
+        }
     }
 }
